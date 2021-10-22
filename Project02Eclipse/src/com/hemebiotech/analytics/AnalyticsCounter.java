3d732c5fa9264b0d073a5a11fe91eb4c60a44312
Tox- <gau.vui97@hotmail.com>
2021-10-22 11:46:00 +0200

    access to input file with relative path

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -13,31 +13,16 @@ import java.util.stream.Stream;
 
 public class AnalyticsCounter {
 
-	public static void main(String args[]) throws Exception {
-		String inFile = "symptoms.txt";
+	public static void main(String[] args) throws Exception {
+		String inFile = System.getProperty("user.dir") + System.getProperty("file.separator") + "Project02Eclipse/symptoms.txt";
 		String oFile = "./result.out";
 
 		Map<String, Integer> symptomsMap = null;
 
 		try (FileWriter writer = new FileWriter (oFile)) {
-			try (Stream<Path> walk = Files.walk(Paths.get(System.getProperty("user.dir")))) {
-				Optional<Path> optPath = walk
-						.filter(p -> p.getFileName().toString().contains(inFile))
-						.findFirst();
-
-				if (optPath.isEmpty()) {
-					String errMess = "Input file not found";
-					writer.write(errMess);
-					System.out.println(errMess);
-					return;
-				}
-
-
-				try (BufferedReader bReader = new BufferedReader(new FileReader(optPath.get().toFile()));) {
-					symptomsMap = new SymptomReaderImpl(bReader).GetSymptoms();
-					writer.write(new AnalyticsSerializer(symptomsMap).serialize());
-				}
-
+			try (BufferedReader bReader = new BufferedReader(new FileReader(inFile))) {
+				symptomsMap = new SymptomReaderImpl(bReader).GetSymptoms();
+				writer.write(new AnalyticsSerializer().serialize(symptomsMap));
 			} catch (Exception ex) {
 				writer.write(ex.toString());
 			}
