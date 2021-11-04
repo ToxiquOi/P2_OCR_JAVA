8a6f3bc9297fb57598c199f84a8c5767a32ea64d
Tox- <gau.vui97@hotmail.com>
2021-11-04 13:29:46 +0100

    Sorted serializer implementing interface

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomSerializer.java
@@ -0,0 +1,15 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+/**
+ *
+ */
+public interface ISymptomSerializer {
+
+    /**
+     * Serialize content of a map
+     * @return Formated Map content
+     */
+    String serialize();
+}
